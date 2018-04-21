b84247f343cccfd38b95b2430159427f01a94e64
trevortomesh <trevor.tomesh@gmail.com>
2018-04-21 02:32:15 -0600

    added clock library and some basic timing routines

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,13 @@
+#include <Time.h>
 #include <WaveHC.h>
 #include <WaveUtil.h>
+#include <TimeLib.h>
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
+
+
+//#include <Time.h>
 //initializing the intput and output pins
 const int lightPin = A3;
 const int moisturePin = A2; 
@@ -75,10 +82,11 @@ void setup() {
   //set pins to output
   pinMode(redpin, OUTPUT);
   pinMode(greenpin, OUTPUT);
+  Serial.begin(9600);// Initialize serial communication  // set up Serial library at 9600 bps
+  setSyncProvider( requestSync);  //set function to call when sync required
+  Serial.println("Waiting for sync message");
 
 
- Serial.begin(9600);// Initialize serial communication  // set up Serial library at 9600 bps
-  
   //reference part:
   //**********************************************
   PgmPrintln("Pi speaker");
@@ -109,21 +117,31 @@ void loop() {
   
   int con=0;//this variable counts condition problem,when a new problem appear con++
 
-  
   if(tks1==0)//if it is just opened go in
   {
     tks1=1;
     playcomplete("cb1.wav");//introduce itself
-    playcomplete("cb2.wav");
     delay(500);
     
     conditionOut(light,soild,temperature);//output all three conditions
   }
+
+    if (Serial.available()) {
+    processSyncMessage();
+  }
+  if (timeStatus()!= timeNotSet) {
+    digitalClockDisplay();  
+  }
+  else{
+    Serial.println("Time Not Set!");
+  }
+  delay(1000);
   
   
   //if light problem appear go in
   if(light==3)
   {
+    Serial.println("Too bright!");
     playcomplete("Lhigh.wav");
     con++;
     tks=1;
@@ -131,12 +149,14 @@ void loop() {
   //if soil problem appear go in
   if(soild==0)
   {
+    Serial.println("Water Me!");
     playcomplete("Sdry.wav");
     con++;
     tks=1;
   }
   else if(soild==3)
   {
+    Serial.println("Overwatered!");
     playcomplete("Swet.wav");
     con++;
     tks=1;
@@ -145,6 +165,7 @@ void loop() {
 
   if(temperature<15||temperature>30)//if temperature problem appear go in
   {
+    Serial.println("Abnormal Temperature!");
     playcomplete("Tbad.wav");
     con++;
     tks=1;
@@ -186,10 +207,7 @@ void loop() {
     //blink red
 
   }
-
-  
-   
-   
+ 
    delay(500);
 }
 
@@ -328,5 +346,47 @@ void numberSp(int i)
   else if (i==18){playcomplete("18.wav");}
   else if (i==19){playcomplete("19.wav");}
   
+}
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
+  }
+  
+}
+
+
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
 }
  
